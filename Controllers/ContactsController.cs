ca24d7e6c67e49406b901ba286f25663c90b87a0
Hlynur Tryggvason <hlynur.tryggvason@officeatwork.com>
2023-08-31 12:06:46 +0200

    feat: support searchByFields

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -1,56 +1,95 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
 namespace CustomExternalListWebApi.Controllers
 {
     [Route("api/contacts")]
     public class MsAppsController : Controller
     {
-        private readonly Contact[] allApps;
+        private readonly Dictionary<string, string>[] allContacts;
 
         public MsAppsController()
         {
             string json = System.IO.File.ReadAllText("data.json");
             var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
-            allApps = JsonSerializer.Deserialize<Contact[]>(json, options);
+            allContacts = JsonSerializer.Deserialize<Dictionary<string, string>[]>(json, options);
         }
 
         public IActionResult Get()
         {
-            return Json(allApps);
+            return Json(allContacts);
         }
 
         [Authorize]
         [HttpPost]
         [RequiredScope("access_as_user")]
-        public IActionResult Post([FromBody] Payload payload)
+        public async Task<IActionResult> Post()
         {
-            switch (payload.Operation)
+            using StreamReader reader = new(Request.Body, Encoding.UTF8);
+            string json = await reader.ReadToEndAsync();
+            var payload = JsonSerializer.Deserialize<Dictionary<string, JsonValue>>(json);
+
+            var operation = payload["operation"].Deserialize<string>();
+
+            switch (operation)
             {
                 case "default":
                     {
-                        var requestedItems = allApps.Where(x =>
-                        {
-                            string idLowerCase = payload.Input.ToLowerInvariant();
-                            return x.Id == idLowerCase;
-                        });
+                        var input = payload["input"].Deserialize<string>();
+                        string idLowerCase = input.ToLowerInvariant();
+
+                        var requestedItems = allContacts.Where(x => x["id"] == idLowerCase);
                         return Json(requestedItems);
                     }
                 case "search":
                     {
-                        var requestedItems = allApps.Where(x =>
-                        {
-                            string searchLowerCase = payload.Input.ToLowerInvariant();
-                            return x.DisplayName.ToLowerInvariant().Contains(searchLowerCase);
-                        }).Select(x => new { x.Id, x.DisplayName, x.City });
+                        var input = payload["input"].Deserialize<string>();
+                        string searchLowerCase = input.ToLowerInvariant();
+
+                        var requestedItems = allContacts
+                            .Where(x => x["displayName"].ToLowerInvariant().Contains(searchLowerCase))
+                            .Select(x => new 
+                            { 
+                                ID = x["id"],
+                                DisplayName = x["displayName"],
+                                City = x["city"]
+                            });
+
+                        return Json(requestedItems);
+                    }
+                case "searchByFields":
+                    {
+                        var input = payload["input"].Deserialize<Dictionary<string, string>>();
+                        var requestedItems = allContacts
+                            .Where(x =>
+                            {
+                                return input.Keys
+                                    .All(key => x[key]
+                                        .ToLowerInvariant()
+                                        .Contains(input[key].ToLowerInvariant()));
+
+                            })
+                            .Select(x => new 
+                            { 
+                                ID = x["id"],
+                                DisplayName = x["displayName"],
+                                City = x["city"]
+                            });
                         return Json(requestedItems);
                     }
                 case "get":
                     {
-                        var requestedItems = allApps.Where(x => payload.Ids.Contains(x.Id));
+                        var ids = payload["ids"].Deserialize<string[]>();
+                        var requestedItems = allContacts.Where(x => ids.Contains(x["id"]));
                         return Json(requestedItems);
                     }
                 default:
--- Payload.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomExternalListWebApi
-{
-    public class Payload
-    {
-        public string Operation { get; set; }
-        public string Input { get; set; }
-        public string[] Ids { get; set; }
-    }
-}
